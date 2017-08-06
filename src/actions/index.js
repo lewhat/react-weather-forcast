40d85217567697bb4d27dbb1c557ef52015c56a3
lewhat <lewhat@live.com>
2017-08-05 22:09:28 -0700

    deploying

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY ='e23921f0b264829f858bf2dc30fe5dab'
-const ROOT_URL =`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL =`https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
